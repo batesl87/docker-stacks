eb55476ce92a80b620b24e47a636b0e8ff2fc6fa
Lee Bates <batesl87@gmail.com>
2017-10-19 15:58:05 +0800

    Update start.sh

--- base-notebook/start.sh
+++ base-notebook/start.sh
@@ -1,10 +1,7 @@
 set -e
 
-spawn /bin/su root
-expect "Password: "
-send "password\n" 
+echo "password" | sudo -S mkdir /mnt/test1
 
-expect "$"
-send "echo \"root:password1\" | chpasswd\n"
+sudo mount -t cifs //dockeraustraliasoutheast.file.core.windows.net/e38f34ea-a82a-47a1-859f-399600cd2ad9 /mnt/test1 -o vers=3.0,username=dockeraustraliasoutheast,password=CHhgeEf13Vpn8V+8rQWvfBFGYU7w5ECyBxmwp35iP+6JSoorLJD1wWywSwwXd2AANLn+kKMbcBq29r1pR/ztrA==,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
 jupyter notebook $*
