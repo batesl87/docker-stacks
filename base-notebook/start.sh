563c1c5723b062703047b2f4b94c8df64160d047
Lee Bates <batesl87@gmail.com>
2017-10-19 16:00:34 +0800

    Update start.sh

--- base-notebook/start.sh
+++ base-notebook/start.sh
@@ -1,7 +1,7 @@
 set -e
 
-echo "password" | sudo -S mkdir /mnt/test1
+echo "password" | sudo -S mkdir /home/jovyan/test1
 
-sudo mount -t cifs //dockeraustraliasoutheast.file.core.windows.net/e38f34ea-a82a-47a1-859f-399600cd2ad9 /mnt/test1 -o vers=3.0,username=dockeraustraliasoutheast,password=CHhgeEf13Vpn8V+8rQWvfBFGYU7w5ECyBxmwp35iP+6JSoorLJD1wWywSwwXd2AANLn+kKMbcBq29r1pR/ztrA==,dir_mode=0777,file_mode=0777,sec=ntlmssp
+sudo mount -t cifs //dockeraustraliasoutheast.file.core.windows.net/e38f34ea-a82a-47a1-859f-399600cd2ad9 /home/jovyan/test1 -o vers=3.0,username=dockeraustraliasoutheast,password=CHhgeEf13Vpn8V+8rQWvfBFGYU7w5ECyBxmwp35iP+6JSoorLJD1wWywSwwXd2AANLn+kKMbcBq29r1pR/ztrA==,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
 jupyter notebook $*
