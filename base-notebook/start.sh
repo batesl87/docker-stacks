6c05f3eb7ac0b689283a1e8704b4c16e739a66e3
Lee Bates <batesl87@gmail.com>
2017-10-20 14:22:11 +0800

    Update start.sh

--- base-notebook/start.sh
+++ base-notebook/start.sh
@@ -4,7 +4,7 @@ echo "password" | sudo -S mkdir /mnt/test1
 
 #sudo mount -t cifs //dockeraustraliasoutheast.file.core.windows.net/e38f34ea-a82a-47a1-859f-399600cd2ad9 /mnt/test1 -o vers=3.0,username=dockeraustraliasoutheast,password=CHhgeEf13Vpn8V+8rQWvfBFGYU7w5ECyBxmwp35iP+6JSoorLJD1wWywSwwXd2AANLn+kKMbcBq29r1pR/ztrA==,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
-sudo deluser $NB_USER sudo
+#sudo deluser $NB_USER sudo
 
 #echo "password" | sudo -S mount -t cifs //dockeraustraliasoutheast.file.core.windows.net/e38f34ea-a82a-47a1-859f-399600cd2ad9 /home/jovyan/test1 -o vers=3.0,username=dockeraustraliasoutheast,password=CHhgeEf13Vpn8V+8rQWvfBFGYU7w5ECyBxmwp35iP+6JSoorLJD1wWywSwwXd2AANLn+kKMbcBq29r1pR/ztrA==,dir_mode=0777,file_mode=0777,sec=ntlmssp
 
