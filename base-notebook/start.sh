48d00986d362613b005d8d7925c3588f033d66f3
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2017-12-08 10:10:44 +0800

    Added custom css for env

--- base-notebook/start.sh
+++ base-notebook/start.sh
@@ -5,8 +5,6 @@ export JUPYTER_TOKEN="$2"
 
 shift 2
 
-$(echo $NV_DOMAIN | cut -f3 -d/)
-
 wget  -O /home/$NB_USER/.jupyter/custom/ "https://github.com/SnowdenNeuroverse/docker-stacks/tree/master/themes/$(echo $NV_DOMAIN | cut -f3 -d/)/custom.css"
 wget  -O /home/$NB_USER/.jupyter/custom/ "https://github.com/SnowdenNeuroverse/docker-stacks/tree/master/themes/$(echo $NV_DOMAIN | cut -f3 -d/)/logo.png"
 
