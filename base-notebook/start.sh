6115f8c1c61c545ca9dc70603fc31b0df6b70aca
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2017-12-08 11:04:31 +0800

    Update start.sh

--- base-notebook/start.sh
+++ base-notebook/start.sh
@@ -5,10 +5,12 @@ export JUPYTER_TOKEN="$2"
 
 shift 2
 
+echo 'create dir'
 mkdir -p /home/$NB_USER/.jupyter/custom/
 
-
+echo 'download css'
 wget  -O /home/$NB_USER/.jupyter/custom/ "https://github.com/SnowdenNeuroverse/docker-stacks/tree/master/themes/$(echo $NV_DOMAIN | cut -f3 -d/)/custom.css"
+echo 'download logo'
 wget  -O /home/$NB_USER/.jupyter/custom/ "https://github.com/SnowdenNeuroverse/docker-stacks/tree/master/themes/$(echo $NV_DOMAIN | cut -f3 -d/)/logo.png"
 
 jupyter notebook $*
