72993edba26fbded4edc4c9541b81126257c0ffa
SnowdenNeuroverse <33819384+SnowdenNeuroverse@users.noreply.github.com>
2018-02-23 14:41:41 +0800

    Update start.sh

--- startup-notebook/start.sh
+++ startup-notebook/start.sh
@@ -1,6 +1,6 @@
 set -e
 
-pip install git+https://github.com/SnowdenNeuroverse/NeuroPython.git@master --upgrade
-julia -e 'Pkg.update("NeuroJulia")'
+#pip install git+https://github.com/SnowdenNeuroverse/NeuroPython.git@master --upgrade
+#julia -e 'Pkg.update("NeuroJulia")'
 
 . /usr/local/bin/start-jupyter.sh $*
